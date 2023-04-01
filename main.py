906d57e209350ef1c5cbf32b48bfaaae662bab91
William <wc187.duke.edu>
2023-04-01 17:32:04 -0400

    current-main branch merge

--- main.py
+++ main.py
@@ -9,14 +9,45 @@ load_dotenv()
 openai.api_key = os.getenv('API_Key')
 
 # basic streamlit design
-st.title('Testing design')
-user_input = st.text_input('enter a user_input')
+import streamlit as st
+
+st.title('AI Spotify playlist generator')
+
+with st.sidebar:
+    Mood = st.slider(
+        'Around what would you rate your mood from 0 (Sad/Somber) to 1 (Happy/Cheerful)?',
+        0.0, 1.0, (0.2, 0.5))
+
+    Tempo = st.slider(
+        'What Tempo music are you looking for?',
+        0.0, 150.0, (25.0, 75.0))
+    
+    status = st.radio('Select the type of music you want: ',
+		('Instrumental', 'Vocal'))
+    options = st.radio('options', ('High Energy', 'Relaxed/Mellow'), label_visibility='collapsed')
+    
+
+    if st.button('Save Configuration'):
+        st.write('We will try to find music that matches your preferences:')
+        st.write('Mood: ', Mood)
+        st.write('Tempo: ', Tempo)
+        st.write('Type: ', options, status, " music")
+        Mud = Mood
+        Tump = Tempo
+        Stump = status
+        Optump = options
+
+    else:
+        st.write('Please save a configuration')
+        
+
+user_input = st.text_input('What would you like to listen to?')
 
 # takes user prompt into gpt
 gpt_response = openai.Completion.create(
   model="text-davinci-003",
-  prompt="provide a spotify song list for" + user_input,
-  max_tokens=500,
+  prompt="provide a list of spotify track URI related to" + user_input,
+  max_tokens=1000,
   temperature=1.5
 )
 
@@ -31,4 +62,6 @@ image_url = image['data'][0]['url']
 st.image(
     image_url,
     )
-st.write(gpt_response['choices'][0]['text'])
+
+song_URIs = gpt_response['choices'][0]['text'].splitlines()
+
