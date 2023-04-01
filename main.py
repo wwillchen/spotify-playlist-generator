de76228f597dc60d670bab72889fa792c85e46bb
William <wc187.duke.edu>
2023-04-01 14:10:15 -0400

    basic streamlit design

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ import streamlit as st
 import pandas as pd
 import config
 
+# basic streamlit design
+st.title('Testing design')
+user_input = st.text_input('enter a user_input')
+st.write('the user input is', user_input)
